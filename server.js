3ae4c0533ced9e1a09520f377fc185ce547a2dd7
REX_HOLMSTROM <rexholmstrom96@gmail.com>
2020-08-09 19:51:42 +0200

    2ndserversave

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const articleRouter = require('./routes/articles')
 const app = express()
+
+//writing code in ejs -> converted to view engine
+app.set('view engine', 'ejs')
+
+//cmd to use the Article router
+//the '/articles' <-- is to place everything in /articles
+app.use('/articles', articleRouter)
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 app.listen(5000)
