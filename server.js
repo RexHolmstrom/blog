181591dbb9c16c34fc9d5483db3b5610f14c757c
REX_HOLMSTROM <rexholmstrom96@gmail.com>
2020-08-10 20:23:30 +0200

    routerupdate

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
 
 //writing code in ejs -> converted to view engine
 app.set('view engine', 'ejs')
@@ -8,6 +11,7 @@ app.set('view engine', 'ejs')
 //cmd to use the Article router
 //the '/articles' <-- is to place everything in /articles
 app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     const articles = [{
