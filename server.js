b31de1f34ec01fb18571c6e343bea18d91f3c42b
REX_HOLMSTROM <rexholmstrom96@gmail.com>
2020-08-10 18:53:08 +0200

    article

--- server.js
+++ server.js
@@ -10,7 +10,19 @@ app.set('view engine', 'ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const articles = [{
+        title: 'test article',
+        createdat: Date.now(),
+        description: 'test description'
+
+    },
+    {
+        title: 'test article',
+        createdat: Date.now(),
+        description: 'test description'
+
+    }]
+    res.render('index', { articles: articles })
 })
 
 app.listen(5000)
