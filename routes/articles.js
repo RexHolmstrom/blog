e96f36f2510d4675d8ddc63d5de2c8af8ecc3e65
REX_HOLMSTROM <rexholmstrom96@gmail.com>
2020-08-13 20:08:25 +0200

    delete btn finish

--- routes/articles.js
+++ routes/articles.js
@@ -31,10 +31,11 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.delete('/', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
 
 
 module.exports = router
