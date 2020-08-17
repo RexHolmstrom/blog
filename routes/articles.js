c0ba83fff033fe78f6e0ab33afc7f0a4a13e825e
REX_HOLMSTROM <rexholmstrom96@gmail.com>
2020-08-17 14:58:28 +0200

    editfix

--- routes/articles.js
+++ routes/articles.js
@@ -21,25 +21,38 @@ router.get('/:slug', async (req, res) => {
 })
 //this is connected to mongoose
 //creating new article 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown,
-    })
-    try {
-        article = await article.save()
-        //redirecting to //articles/id
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        res.render('articles/new', { article: article })
-    }
-})
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+
+        try {
+            article = await article.save()
+            //redirecting to //articles/id
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            res.render(`articles/new/${path}`, { article: article })
+        }
+
+    }
+}
 
 
 
